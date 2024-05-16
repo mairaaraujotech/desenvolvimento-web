f306fe69f342cba4da9beae2e66abe1e5c683f6c
Maira <maira.araujo.tech@gmail.com>
2024-05-16 14:15:28 -0300

    logica-js-projeto Aula 03

--- javascript/logica-js-projeto_inicial/logica-js-projeto_inicial/app.js
+++ javascript/logica-js-projeto_inicial/logica-js-projeto_inicial/app.js
@@ -1,10 +1,21 @@
+let numeroSecreto = 5;
+let chute
+let tentativas = 1;
+
 alert('Seja bem-vindo ao jogo do número secreto');
 
-let numeroSecreto = 5;
-let chute = prompt('Escolha um número entre 1 e 10');
+while (chute != numeroSecreto) {
+    
+    chute = prompt('Escolha um número entre 1 e 10');
 
-if (chute == numeroSecreto) {
-    alert(`Você descobriu o número secreto: ${numeroSecreto}`);
-} else {
-    alert('Você errou, tente outra vez...')
+    if (chute == numeroSecreto) {
+        alert(`Você descobriu o número secreto: ${numeroSecreto} com ${tentativas} tentativas`);
+    } else {
+        if (chute > numeroSecreto) {
+            alert(`O número secreto é menor que ${chute}`);
+        } else {
+            alert(`O número secreto é maior que ${chute}`);
+        }
+        tentativas++
+    }
 }
