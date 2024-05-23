d99265906dfa438b9ba0205eb888e58bfd7b5c96
Maira <maira.araujo.tech@gmail.com>
2024-05-23 15:37:59 -0300

    js-inserindo-html Criando

---
+++ javascript/js-inserindo-html/base.js
@@ -0,0 +1,12 @@
+let titulo = document.querySelector('.cabecalho__titulo');
+titulo.innerHTML = 'Portal de Notícias';
+
+let subtitulo = document.querySelector('.cabecalho__subtitulo');
+subtitulo.innerHTML = 'Fique por dentro do que acontece no mundo';
+
+let desenvolvedor = document.querySelector('.desenvolvedor');
+desenvolvedor.innerHTML = 'Desenvolvido por Maira Araújo';
+
+let localAno = document.querySelector('.local-ano');
+localAno.innerHTML = 'Rio de Janeiro | 2024';
+
