dd546164411dba6e8341592899ab6d2042bd5262
Maira <maira.araujo.tech@gmail.com>
2024-05-23 11:56:17 -0300

    js-listas-funcoes Aula 02

--- javascript/js-listas-funcoes/app.js
+++ javascript/js-listas-funcoes/app.js
@@ -1,9 +1,25 @@
-let titulo = document.querySelector('h1');
-titulo.innerHTML = 'Jogo do Número Secreto';
+// let titulo = document.querySelector('h1');
+// titulo.innerHTML = 'Jogo do Número Secreto';
 
-let paragrafo = document.querySelector('p');
-paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
+// let paragrafo = document.querySelector('p');
+// paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
+
+let numeroSecreto = gerarNumeroAleatorio();
+
+function exibirTextoNaTela(tag, texto) {
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+}
 
 function verificarChute() {
-    console.log('O botão foi clicado')
+    let chute = document.querySelector('input').value;
+    console.log(numeroSecreto);
+    console.log(chute == numeroSecreto);
 }
+
+function gerarNumeroAleatorio() {
+    return parseInt(Math.random() * 10 + 1);
+}
+
+exibirTextoNaTela('h1', 'Jogo do Número Secreto')
+exibirTextoNaTela('p', 'Escolha um número entre 1 e 10')
