69a70792c3770d1873d93d14f78971364a7e93b3
Maira <maira.araujo.tech@gmail.com>
2024-07-22 19:02:22 -0300

    JS praticando-logica Aula 05

---
+++ javascript/praticando-logica/amigo-secreto/js/app.js
@@ -0,0 +1,37 @@
+let amigos = [];
+
+function adicionar() {
+    let amigo = document.getElementById('nome-amigo');
+    let lista = document.getElementById('lista-amigos');
+    amigos.push(amigo.value);
+    if (lista.textContent == '') {
+        lista.textContent = amigo.value;
+    } else {
+        lista.textContent = lista.textContent + ', ' + amigo.value;
+    }
+    amigo.value = '';
+}
+//INCLUIR FUNCIONALIDADES: 1. não pode aceitar nome repetido; 2. não pode aceitar nome em branco; 3. precisa ter uma quantidade mínima de participantes
+function sortear() {
+    embaralha(amigos);
+    let sorteio = document.getElementById('lista-sorteio');
+    for (let i = 0; i < amigos.length; i++) {
+        if (i == amigos.length - 1) {
+            sorteio.innerHTML = sorteio.innerHTML + amigos[i] + ' --> ' + amigos[0] + '<br>';
+        } else {
+            sorteio.innerHTML = sorteio.innerHTML + amigos[i] + ' --> ' + amigos[i + 1] + '<br>';
+        }
+    }
+}
+function embaralha(lista) {
+    for (let indice = lista.length; indice; indice--) {
+        const indiceAleatorio = Math.floor(Math.random() * indice);
+        [lista[indice - 1], lista[indiceAleatorio]] =
+        [lista[indiceAleatorio], lista[indice - 1]];
+    }
+}
+function reiniciar() {
+    amigos = [];
+    document.getElementById('lista-amigos').innerHTML = '';
+    document.getElementById('lista-sorteio').innerHTML = '';
+}
