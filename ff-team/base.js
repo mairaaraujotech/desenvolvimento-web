891adddbf4df0a539e84e69ac0731539f4b99d8b
Maira <maira.araujo.tech@gmail.com>
2024-10-04 15:00:31 -0300

    ff-team

--- ff-team/base.js
+++ ff-team/base.js
@@ -2,8 +2,8 @@ function exibirTexto(tag, texto) {
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
 }
-exibirTexto('.cabecalho__titulo', 'Portal de Notícias');
-exibirTexto('.cabecalho__subtitulo', 'Fique por dentro do que acontece no mundo');
+exibirTexto('.cabecalho__titulo', 'FFteam');
+exibirTexto('.cabecalho__subtitulo', 'Consultoria Esportiva');
 exibirTexto('.desenvolvedor', 'Desenvolvido por ');
 exibirTexto('.rodape__link', 'Maira Araújo');
 exibirTexto('.local-ano', 'Rio de Janeiro | 2024');
