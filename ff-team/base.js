8181bcdd36a647e3fc5dc75ac392896d1a072a3d
mairaaraujotech <maira.araujo.tech@gmail.com>
2026-02-24 15:41:01 -0300

    FFTeam
    Reorganização de estilo.

--- ff-team/base.js
+++ ff-team/base.js
@@ -4,6 +4,9 @@ function exibirTexto(tag, texto) {
 }
 exibirTexto('.cabecalho__titulo', 'FFteam');
 exibirTexto('.cabecalho__subtitulo', 'Consultoria Esportiva');
+exibirTexto('.botao-voltar', 'Voltar');
 exibirTexto('.desenvolvedor', 'Desenvolvido por ');
 exibirTexto('.rodape__link', 'Maira Araújo');
+exibirTexto('.hospedagem', 'Hospedado gratuitamente no ');
+exibirTexto('.git-hub__link', 'GitHub');
 exibirTexto('.local-ano', 'Rio de Janeiro | 2024');
