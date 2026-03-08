c0f17935aaa23a265494fd2ba1d617e85eb4526c
mairaaraujotech <maira.araujo.tech@gmail.com>
2026-03-07 22:20:28 -0300

    FFTeam e FitTeam Gradim
    Projeto base.

---
+++ fitteamgradim/basic.js
@@ -0,0 +1,20 @@
+function exibirTexto(tag, texto) {
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+}
+function exibirImagem(tag, imagem){
+    let campo = document.querySelector(seletor);
+    elemento.innerHTML = `<img src="${caminho}" alt="${descricao}">`;
+}
+exibirImagem('#imagem-home', 'home.png', 'Logo FFteam');
+exibirTexto('#botao-voltar', '❰❰❰');
+exibirTexto('#desenvolvedor', 'Desenvolvido por ');
+exibirTexto('#desenvolvedor-link', 'Maira Araújo');
+exibirTexto('#hospedagem', 'Hospedado gratuitamente no ');
+exibirTexto('#github-link', 'GitHub');
+exibirTexto('#local-ano', 'Rio de Janeiro | 2024');
+
+function exibirImagem(seletor, caminho, descricao) {
+    let elemento = document.querySelector(seletor);
+    elemento.innerHTML = `<img src="${caminho}" alt="${descricao}">`;
+}
